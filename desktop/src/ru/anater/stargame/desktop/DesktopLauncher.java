eed02ebc1590f0cd6172d2ef6d7a324224eac93d
anater <anater78@gmail.com>
2019-12-11 12:10:52 +0300

    Initial commit

---
+++ desktop/src/ru/anater/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.anater.stargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.anater.stargame.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
